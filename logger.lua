ddaa187321f1d1603129155de397a0ed61e3d8e9
mvousden <mark.vousden@soton.ac.uk>
2017-09-25 22:16:24 +0100

    master: More progress on logger.

--- logger.lua
+++ logger.lua
@@ -1,6 +1,8 @@
 -- Defines a simple singleton logging class, where the logger can be obtained,
 -- and created if necessary, by calling getLogger.
---
+
+local logLevels = {"DEBUG", "INFO", "WARNING", "ERROR"}
+
 -- This logger has four levels: DEBUG (1), INFO (2), WARNING (3), and ERROR
 -- (4). Log entries are written by calling one of the "debug", "info"
 -- etc. methods from the logger table.
@@ -14,9 +16,43 @@ local logFileLevel = 1  -- Sets the level below which log entries are not
                         -- printed to the log file.
 local logStdoutLevel = 1  -- Likewise for the standard output.
 
--- Logger behaviour
+
+-- Writing functions
+function write_message_to_file(message, logFile, level)
+   -- Writes a log message to a file. Arguments:
+   --   message: String denoting message to write.
+   --   logFile: File object to write to.
+   --   level: Integer denoting the log level.
+   -- Returns nothing.
+   logFile:write(message_to_logline(message .. "\n", level))
+end
+
+
+function write_message_to_stdout(message, level)
+   -- Writes a log message to standard output. Arguments:
+   --   message: String denoting message to write.
+   --   level: Integer denoting the log level.
+   -- Returns nothing.
+   print(message_to_logline(message, level))
+end
+
+
+function message_to_logline(message, level)
+   -- Converts a logging message and a logging level to a line entry to be
+   -- written either to a file or to the standard output. Arguments:
+   --   message: String denoting message to write.
+   --   level: Integer denoting the log level.
+   -- Returns the line entry as a string.
+
+   return string.format("[%s] %s: %s", os.date("%H:%M:%S"),
+                        logLevels[level], message)
+end
+
+
+-- Logger class behaviour
 Logger = {}
 Logger.__index = Logger
+
 
 function Logger:create()
    local logger = {}
@@ -27,10 +63,31 @@ function Logger:create()
       logger.logFile = io.open(logPath, "a")
    end
 
-   -- Define logging functions from the levels.
+   -- Define logging functions from the levels. Iterate over each log level and
+   -- "predefine" a logging function for that level.
+   for id, level in ipairs(logLevels) do
+      if logFileLevel > id and logStdoutLevel > id then
+         logger[string.lower(level)] = function(message) end
+      elseif logFileLevel > id and logStdoutLevel <= id then
+         logger[string.lower(level)] = function(message)
+            write_message_to_stdout(message, id)
+         end
+      elseif logFileLevel <= id and logger.logFile and logStdoutLevel > id then
+         logger[string.lower(level)] = function(message)
+            write_message_to_file(message, logger.logFile, id)
+         end
+      elseif logFileLevel <= id and logger.logFile and logStdoutLevel <= id then
+         logger[string.lower(level)] = function(message)
+            print(string.format("Printing %s", message))
+            write_message_to_stdout(message, id)
+            write_message_to_file(message, logger.logFile, id)
+         end
+      end
+   end
 
    return logger
 end
+
 
 function Logger:__gc()
    getLogger():debug("Log closed.")
@@ -52,6 +109,12 @@ function getLogger()
       return _logger
    else
       _logger = Logger:create()
-      getLogger():debug("Log opened.")
+      for key, value in pairs(_logger) do print(key, value) end
+      _logger:debug("Log opened.")
+      return _logger
    end
 end
+
+
+-- Module table
+return {getLogger=getLogger}
